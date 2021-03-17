60d0f8101764115f3c29ae73c1d7fb49724dc2e7
Sergey Lemeshevsky <sergey.lemeshevsky@gmail.com>
2021-03-17 22:16:11 +0300

    Добавлено построение графиков

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ import seaborn as sns
 import pandas as pd
 import patsy as pt
 import sklearn.linear_model as lm
-import statsmodels.api as sm
+# import statsmodels.api as sm
 
 
 data, country = read('Belarus', with_recovered=True)
@@ -17,13 +17,25 @@ data['I'] = data['X'] - data['Recovered']  # - data['Deaths']
 data['alpha'] = data['dR']/data['I']
 data['dXbyI'] = data['Delta']/data['I']
 
-regr_data = data[(data['X'] < 52000) & (data['dXbyI'].notnull())]
+
+left = 10000
+right = 65000
+regr_data = data[(data['X'] > left) & (data['X'] < right)]
 x = regr_data['X'].values.reshape(-1, 1)
 y = regr_data['dXbyI'].values
 skm = lm.LinearRegression()
 skm.fit(x, y)
-print(skm.intercept_, skm.coef_)
+beta = -skm.coef_[0]
+N = skm.intercept_/beta
 
+fig, ax = plt.subplots()
+regr_data[['X', 'dXbyI']].plot(x='X', ax=ax)
+x = regr_data['X'].values
+y = -beta*x + beta*N
+ax.plot(x, y)
+ax.legend(['$\\frac{\dot{X}}{I}$', '$-\\beta X + \\beta N$'])
+plt.title('Беларусь: $\\beta = {0:.8f}$, $N = {1:.0f}$'.format(beta, N))
+plt.savefig('results/Belarus_{0}_{1}.pdf'.format(left, right))
 
 #fig, ax = plt.subplots()
 #fig.suptitle(country)
