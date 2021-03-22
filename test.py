54b883a2c371275beb405a5df1b2d5d5a8b6293a
Artsiom Aulas <aRToLoMiay@tut.by>
2021-03-22 18:40:08 +0300

    Построение регрессии для характеристик по Беларуси.

--- test.py
+++ test.py
@@ -12,30 +12,72 @@ import sklearn.linear_model as lm
 # import statsmodels.api as sm
 
 
-data, country = read('Belarus', with_recovered=True)
+country = 'Belarus'
+# Getting data from csv.
+data, country = read(country, with_recovered=True)
 data['I'] = data['X'] - data['Recovered']  # - data['Deaths']
 data['alpha'] = data['dR']/data['I']
 data['dXbyI'] = data['Delta']/data['I']
-
-
+# Data preapatarion.
 left = 10000
 right = 65000
+data = data.rolling(7).mean().dropna()
 regr_data = data[(data['X'] > left) & (data['X'] < right)]
+
+# Beta
+# Linear regression.
 x = regr_data['X'].values.reshape(-1, 1)
 y = regr_data['dXbyI'].values
 skm = lm.LinearRegression()
 skm.fit(x, y)
 beta = -skm.coef_[0]
 N = skm.intercept_/beta
-
+# Plotting.
 fig, ax = plt.subplots()
 regr_data[['X', 'dXbyI']].plot(x='X', ax=ax)
 x = regr_data['X'].values
-y = -beta*x + beta*N
+y = beta * (N - x) # -beta*x + beta*N
 ax.plot(x, y)
 ax.legend(['$\\frac{\dot{X}}{I}$', '$-\\beta X + \\beta N$'])
 plt.title('Беларусь: $\\beta = {0:.8f}$, $N = {1:.0f}$'.format(beta, N))
-plt.savefig('results/Belarus_{0}_{1}.pdf'.format(left, right))
+plt.savefig('results/Belarus_dXbyI_{0}_{1}.pdf'.format(left, right))
+
+# Alpha (cut data)
+# Linear regression.
+x = regr_data['X'].values.reshape(-1, 1)
+y = regr_data['alpha'].values
+skm = lm.LinearRegression()
+skm.fit(x, y)
+beta0 = skm.intercept_
+beta1 = skm.coef_[0]
+# Plotting.
+fig, ax = plt.subplots()
+regr_data[['X', 'alpha']].plot(x='X', ax=ax)
+x = regr_data['X'].values
+y = beta1 * x + beta0
+ax.plot(x, y)
+ax.legend(['$\\frac{\dot{R}}{I}$', '$\\alpha = \\beta_0 + \\beta_1 x$'])
+plt.title('Беларусь: $\\beta_0 = {0:.8f}$, $\\beta_1 = {1:.8f}$'.format(beta0, beta1))
+plt.savefig('results/Belarus_alpha_{0}_{1}.pdf'.format(left, right))
+
+# Alpha (all data)
+# Linear regression.
+x = data['X'].values.reshape(-1, 1)
+y = data['alpha'].values
+skm = lm.LinearRegression()
+skm.fit(x, y)
+beta0 = skm.intercept_
+beta1 = skm.coef_[0]
+# Plotting.
+fig, ax = plt.subplots()
+data[['X', 'alpha']].plot(x='X', ax=ax)
+x = data['X'].values
+y = beta1 * x + beta0
+ax.plot(x, y)
+ax.legend(['$\\frac{\dot{R}}{I}$', '$\\alpha = \\beta_0 + \\beta_1 x$'])
+plt.title('Беларусь: $\\beta_0 = {0:.8f}$, $\\beta_1 = {1:.8f}$'.format(beta0, beta1))
+plt.savefig('results/Belarus_alpha_full.pdf')
+
 
 #fig, ax = plt.subplots()
 #fig.suptitle(country)
